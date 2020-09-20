6aec7c7e7b7ec901c59bd57edfeed1a7f35d6dc3
Snehal Kadwe <snehalkadwe@gmail.com>
2020-09-21 00:51:11 +0530

    s

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -71,6 +71,7 @@ class TodoListController extends Controller
 // https://www.youtube.com/watch?v=gyWLxpYWxFQ
 // https://www.youtube.com/watch?v=2bslfjKEAik
 // https://laravel.com/docs/8.x/authentication
+// https://laravel.com/docs/8.x/authentication
 
 
 // <form action={{ route('tasks.update', $task->id) }} method="POST">
